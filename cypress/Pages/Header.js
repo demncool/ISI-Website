a0472b34f65422a49838f88091f2bcd54eb93642
Bryan James Rodrigo <Bryan James Rodrigo@DESKTOP-KORKLJ8>
2023-07-04 15:11:16 +0800

    added header scripts

---
+++ cypress/Pages/Header.js
@@ -0,0 +1,24 @@
+export const Header = {
+    Verify_if_Header_contains_all_elements: (navigationText) => {
+        cy.get(".navbar-nav > li")
+            .as("element")
+            .each((el, index, list) => {
+                cy.wrap(el).should("contain", navigationText[index])
+            })
+    },
+
+    Verify_if_Navigation_is_Working: function (navigationLinks) {
+        cy.get(".navbar-nav > li")
+            .as("element")
+            .each((el, index, list) => {
+                cy.get("@element")
+                    .eq(index)
+                    .then((val) => {
+                        cy.wrap(val)
+                            .click()
+                            .url()
+                            .should("include", navigationLinks[index])
+                    })
+            })
+    }
+}
---
+++ cypress/e2e/Header.cy.js
@@ -0,0 +1,36 @@
+import { Header } from "../Pages/Header"
+
+describe("Home", () => {
+    beforeEach(() => {
+        cy.visit('/')
+        cy.acceptCookies()
+    })
+    it("Verify if Header contains all elements", () => {
+        const navigationText = [
+            "Home",
+            "About",
+            "Services",
+            "Projects",
+            "Careers",
+            "Blog",
+            "Contact Us"
+        ]
+
+        cy.get("#isi-logo").should("be.visible")
+        Header.Verify_if_Header_contains_all_elements(navigationText)
+    })
+
+    it("Verify if ISI Logo Function Works", () => {
+        cy.get("#isi-logo").click()
+        cy.url().should("eq", "https://dev.innovuze.com/")
+    })
+
+    it.only("Verify if Navigation is Working", function () {
+        cy.fixture("Header").then(function (header) {
+            this.header = header
+        })
+        
+        Header.Verify_if_Navigation_is_Working(this.header.navigationLinks)
+
+    })
+})
